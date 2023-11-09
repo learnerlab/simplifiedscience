333e6adbad98360d64ea914b3c04ac2ba6d2e908
learnerlab <150380296+learnerlab@users.noreply.github.com>
2023-11-09 15:18:19 -0600

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,51 +1,77 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+import os
+import openai
 import streamlit as st
-from streamlit.logger import get_logger
+import fitz  # PyMuPDF
+import io
 
-LOGGER = get_logger(__name__)
+# Attempt to load the OpenAI API key from environment variables
+openai_api_key = os.getenv('OPENAI_API_KEY')
 
+if openai_api_key is None:
+    st.error("OpenAI API key not found. Please set the OPENAI_API_KEY environment variable.")
+else:
+    openai.api_key = openai_api_key
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+def process_question(question, document_content=None):
+    if openai.api_key is None:
+        return "Error: OpenAI API key is not set."
+
+    # Craft the prompt to align with the Simplified Science behavior
+    prompt = (
+        "The following is a question about a scientific topic. "
+        "Provide a clear and simple explanation suitable for a general audience: "
+        f"{question}"
     )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
+    
+    # If a document is uploaded, append its content to the prompt
+    if document_content:
+        prompt += (
+            "\n\nAttached is a scientific paper. Summarize the key points and conclusions in simple terms:\n"
+            f"{document_content}"
+        )
+    
+    # Call the OpenAI API with the crafted prompt
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=500
     )
+    
+    # Extract the text from the response
+    return response.choices[0].text.strip()
 
+def read_pdf(file_data):
+    # Open the PDF file from the uploaded BytesIO object
+    pdf = fitz.open(stream=file_data, filetype="pdf")
+    text = ""
+    # Extract text from each page of the PDF
+    for page in pdf:
+        text += page.get_text()
+    pdf.close()
+    return text
 
-if __name__ == "__main__":
-    run()
+def main():
+    st.title('Simplified Science GPT App')
+    user_question = st.text_input('Enter your question about science here:')
+    
+    # File uploader for PDF documents
+    uploaded_file = st.file_uploader("Upload a scientific paper (optional):", type=['pdf'])
+
+    document_content = None
+    if uploaded_file is not None:
+        # Convert the uploaded file to a BytesIO object
+        file_bytes = io.BytesIO(uploaded_file.read())
+        # Check the uploaded file type and process accordingly
+        if uploaded_file.type == "application/pdf":
+            document_content = read_pdf(file_bytes)
+        else:
+            st.error("Unsupported file type. Please upload a PDF.")
+    
+    if st.button('Submit'):
+        # Use a spinner while generating the response
+        with st.spinner('Generating response...'):
+            answer = process_question(user_question, document_content)
+            st.write(answer)
+
+if __name__ == '__main__':
+    main()
